a4c06da553d7c56767fd2f503647952d4ce85931
tharsikan1999 <tharsikanking@gmail.com>
2024-03-24 20:10:03 +0530

    update

--- components/Home.tsx
+++ components/Home.tsx
@@ -5,20 +5,27 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPlus, faPenToSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 
-export default function Home() {
-    const [notes, setNotes] = useState<any[]>([]);
+interface Note {
+    _id: string;
+    title: string;
+    note: string;
+}
+
+const Home: React.FC = () => {
+    const [notes, setNotes] = useState<Note[]>([]);
     const [formData, setFormData] = useState({
         title: '',
         content: ''
     });
-    const [selectedRowIndex, setSelectedRowIndex] = useState(null);
+    const [selectedRowIndex, setSelectedRowIndex] = useState<number | null>(null);
     const [formErrors, setFormErrors] = useState({
         title: '',
         content: ''
     });
 
-    const [selectRow , setSelectRow] = useState(0);
-
+    useEffect(() => {
+        fetchNotes();
+    }, []);
 
     const fetchNotes = async () => {
         try {
@@ -30,37 +37,18 @@ export default function Home() {
             throw new Error("Failed to get data from MongoDB");
         }
     };
-    
 
-    useEffect(() => {
-        fetchNotes();
-    }, []);
-
-   
-
-    const handleChange = (e) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         let errorMessage = '';
-        if (name === 'title') {
-            if (!value.trim()) {
-                errorMessage = 'Title is required';
-            }
-        } else if (name === 'content') {
-            if (!value.trim()) {
-                errorMessage = 'Content is required';
-            }
+        if (name === 'title' && !value.trim()) {
+            errorMessage = 'Title is required';
+        } else if (name === 'content' && !value.trim()) {
+            errorMessage = 'Content is required';
         }
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-        setFormErrors(prevErrors => ({
-            ...prevErrors,
-            [name]: errorMessage
-        }));
+        setFormData(prevState => ({ ...prevState, [name]: value }));
+        setFormErrors(prevErrors => ({ ...prevErrors, [name]: errorMessage }));
     };
-
-    //add note
 
     const handleAdd = async () => {
         try {
@@ -71,73 +59,60 @@ export default function Home() {
                 });
                 return;
             }
-            const response = await axios.post('http://localhost:3000/api/add', {
+            await axios.post('http://localhost:3000/api/add', {
                 title: formData.title,
                 note: formData.content
             });
             window.alert('Note added successfully');
-            setFormData({
-                title: '',
-                content: ''
-            });
+            setFormData({ title: '', content: '' });
             fetchNotes();
         } catch (error) {
-            if (error instanceof Error) {
-                window.alert(error.message)
-            }
+            console.error('Error adding note:', error);
+            window.alert('Failed to add note');
         }
     };
 
-    //delete note
-
-    const handleDelete = async (index:number) => {
+    const handleDelete = async (index: number) => {
         const shouldDelete = window.confirm("Are you sure you want to delete this note?");
         if (shouldDelete) {
             try {
                 await axios.delete(`http://localhost:3000/api/delete/${notes[index]._id}`);
                 fetchNotes();
             } catch (error) {
-                
                 console.error('Error deleting note:', error);
+                window.alert('Failed to delete note');
             }
         }
     };
 
-    //edit note
-
-    const handleEdit = (index) => {
+    const handleEdit = (index: number) => {
         setFormData({
             title: notes[index].title,
             content: notes[index].note
         });
         setSelectedRowIndex(index);
-        setSelectRow(index);
     };
 
-    //update note
-
-    const handleUpdate = async (index) => {
+    const handleUpdate = async () => {
         try {
-            await axios.put(`http://localhost:3000/api/update/${notes[selectRow]._id}`, {
-                title: formData.title,
-                note: formData.content
-            });
-            window.alert('Note updated successfully');
-            setSelectedRowIndex(null);
-            fetchNotes();
-            setFormData(
-                {
-                    title: '',
-                    content: ''
-                }
-            )
-        } catch (error) {
-            if (error instanceof Error) {
-                window.alert(error.message)
+            if (selectedRowIndex !== null) {
+                await axios.put(`http://localhost:3000/api/update/${notes[selectedRowIndex]._id}`, {
+                    title: formData.title,
+                    note: formData.content
+                });
+                window.alert('Note updated successfully');
+                setSelectedRowIndex(null);
+                fetchNotes();
+                setFormData({ title: '', content: '' });
             }
+        } catch (error) {
+            console.error('Error updating note:', error);
+            window.alert('Failed to update note');
         }
     };
 
+
+    
     return (
         <div className="mx-auto flex items-center  flex-col h-screen py-5  overflow-auto">
             <h1 className="mb-4  text-mycolor text-4xl  font-extrabold leading-none tracking-tight  " >Your Notes</h1>
@@ -163,66 +138,65 @@ export default function Home() {
                 />
                 {formErrors.content && <div className="text-red-500 text-xs">{formErrors.content}</div>}
                 {
-    selectedRowIndex === null ? (
-        <div className='mt-3 bg-mycolor h-10 w-10 rounded-full flex items-center justify-center font-semibold text-white hover:bg-white text-xl hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer' onClick={handleAdd}>
-            <p><FontAwesomeIcon icon={faPlus} /></p>
-        </div>
-    ) : (
-        <div className='mt-3 bg-mycolor h-10 w-10 rounded-full flex items-center justify-center font-semibold text-white hover:bg-white text-xl hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer' onClick={handleUpdate}>
-            <p><FontAwesomeIcon icon={faCheckSquare} /></p>
-        </div>
-    )
-}
-<div className="overflow-x-auto">
-    <table className="mt-20 min-w-full table-auto">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" className="px-4 py-2">
-                    No
-                </th>
-                <th scope="col" className="px-4 py-2">
-                    Title
-                </th>
-                <th scope="col" className="px-4 py-2">
-                    Note
-                </th>
-                <th scope="col" className="px-4 py-2">
-                    Action
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            {notes.map((note:any, index:number) => (
-                <tr className="text-sm" key={index}>
-                    <td className="border px-4 py-2">{index + 1}</td>
-                    <td className="border px-4 py-2">{note.title}</td>
-                    <td className="border px-4 py-2">{note.note}</td>
-                    <td className="border px-4 py-2">
-                        <div className="flex items-center justify-center">
-                            {selectedRowIndex === index ? (
-                                <button className="bg-mycolor h-8 w-8 rounded-full flex items-center justify-center font-semibold text-white ml-2 hover:bg-white hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer" onClick={() => handleUpdate(index)}>
-                                    <FontAwesomeIcon icon={faCheckSquare} />
-                                </button>
-                            ) : (
-                                <>
-                                    <button className="bg-mycolor h-8 w-8 rounded-full flex items-center justify-center font-semibold text-white ml-2 hover:bg-white hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer" onClick={() => handleDelete(index)}>
-                                        <FontAwesomeIcon icon={faTrash} />
-                                    </button>
-                                    <button className="bg-mycolor h-8 w-8 rounded-full flex items-center justify-center font-semibold text-white ml-2 hover:bg-white hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer" onClick={() => handleEdit(index)}>
-                                        <FontAwesomeIcon icon={faPenToSquare} />
-                                    </button>
-                                </>
-                            )}
+                    selectedRowIndex === null ? (
+                        <div className='mt-3 bg-mycolor h-10 w-10 rounded-full flex items-center justify-center font-semibold text-white hover:bg-white text-xl hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer' onClick={handleAdd}>
+                            <p><FontAwesomeIcon icon={faPlus} /></p>
                         </div>
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
-</div>
-
-                
+                    ) : (
+                        <div className='mt-3 bg-mycolor h-10 w-10 rounded-full flex items-center justify-center font-semibold text-white hover:bg-white text-xl hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer' onClick={handleUpdate}>
+                            <p><FontAwesomeIcon icon={faCheckSquare} /></p>
+                        </div>
+                    )
+                }
+                <div className="overflow-x-auto">
+                    <table className="mt-20 min-w-full table-auto">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-4 py-2">
+                                    No
+                                </th>
+                                <th scope="col" className="px-4 py-2">
+                                    Title
+                                </th>
+                                <th scope="col" className="px-4 py-2">
+                                    Note
+                                </th>
+                                <th scope="col" className="px-4 py-2">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {notes.map((note:any, index:number) => (
+                                <tr className="text-sm" key={index}>
+                                    <td className="border px-4 py-2">{index + 1}</td>
+                                    <td className="border px-4 py-2">{note.title}</td>
+                                    <td className="border px-4 py-2">{note.note}</td>
+                                    <td className="border px-4 py-2">
+                                        <div className="flex items-center justify-center">
+                                            {selectedRowIndex === index ? (
+                                                <button className="bg-mycolor h-8 w-8 rounded-full flex items-center justify-center font-semibold text-white ml-2 hover:bg-white hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer" onClick={() => handleUpdate()}>
+                                                    <FontAwesomeIcon icon={faCheckSquare} />
+                                                </button>
+                                            ) : (
+                                                <>
+                                                    <button className="bg-mycolor h-8 w-8 rounded-full flex items-center justify-center font-semibold text-white ml-2 hover:bg-white hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer" onClick={() => handleDelete(index)}>
+                                                        <FontAwesomeIcon icon={faTrash} />
+                                                    </button>
+                                                    <button className="bg-mycolor h-8 w-8 rounded-full flex items-center justify-center font-semibold text-white ml-2 hover:bg-white hover:text-mycolor hover:border-2 hover:border-mycolor hover:cursor-pointer" onClick={() => handleEdit(index)}>
+                                                        <FontAwesomeIcon icon={faPenToSquare} />
+                                                    </button>
+                                                </>
+                                            )}
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
-}
+                                            }
+ export default Home;
--- libs/mongodb.js
+++ libs/mongodb.js
@@ -4,7 +4,11 @@ const connectToMongoDB = async () => {
     try {
         await mongoose.connect("mongodb://127.0.0.1:27017/note", {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            // Optionally, you can add more options here
+            // For example:
+            // useCreateIndex: true,
+            // useFindAndModify: false,
         });
         console.log("Connected to MongoDB");
     } catch (error) {
